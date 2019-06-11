43156c1e4c40a66b3eff22a0785c46077295a848
aagarwal1012 <aagarwal9782@gmail.com>
2019-06-12 02:59:13 +0530

    feat: added annotations in qual

---
+++ src/main/java/org/checkerframework/checker/androidresource/qual/ArrayRes.java
@@ -0,0 +1,16 @@
+package org.checkerframework.checker.androidresource.qual;
+
+import org.checkerframework.framework.qual.*;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@SubtypeOf({AnyRes.class})
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+@TargetLocations({TypeUseLocation.EXPLICIT_LOWER_BOUND, TypeUseLocation.EXPLICIT_UPPER_BOUND})
+@ImplicitFor(literals = LiteralKind.NULL, typeNames = java.lang.Void.class)
+public @interface ArrayRes {
+}

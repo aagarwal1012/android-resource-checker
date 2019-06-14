75e5ac7d3b6a81d5ab1f92734a54087f6749d6c1
aagarwal1012 <aagarwal9782@gmail.com>
2019-06-14 19:41:17 +0530

    fix: add XXXContainer qualifers to ResourceBottom

---
+++ src/main/java/org/checkerframework/checker/androidresource/AndroidResourceAnnotatedTypeFactory.java
@@ -0,0 +1,4 @@
+package org.checkerframework.checker.androidresource;
+
+public class AndroidResourceAnnotatedTypeFactory {
+}
--- src/main/java/org/checkerframework/checker/androidresource/qual/ResourceBottom.java
+++ src/main/java/org/checkerframework/checker/androidresource/qual/ResourceBottom.java
@@ -1,5 +1,6 @@
 package org.checkerframework.checker.androidresource.qual;
 
+import org.checkerframework.checker.androidresource.qual.container.*;
 import org.checkerframework.checker.androidresource.qual.res.*;
 import org.checkerframework.framework.qual.*;
 
@@ -12,7 +13,11 @@ import java.lang.annotation.Target;
         BoolRes.class, ColorRes.class, DimenRes.class, DrawableRes.class, FractionRes.class,
         IdRes.class, IntegerRes.class, InterpolatorRes.class, LayoutRes.class, MenuRes.class,
         PluralsRes.class, RawRes.class, StringRes.class, StyleableRes.class, XmlRes.class,
-        Resource.class, })
+        AnimatorContainer.class, AnimContainer.class, ArrayContainer.class, AttrContainer.class,
+        BoolContainer.class, ColorContainer.class, DimenContainer.class, DrawableContainer.class, FractionContainer.class,
+        IdContainer.class, IntegerContainer.class, InterpolatorContainer.class, LayoutContainer.class, MenuContainer.class,
+        PluralsContainer.class, RawContainer.class, StringContainer.class, StyleableContainer.class, XmlContainer.class,
+        Resource.class,})
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 @TargetLocations({TypeUseLocation.EXPLICIT_LOWER_BOUND, TypeUseLocation.EXPLICIT_UPPER_BOUND})

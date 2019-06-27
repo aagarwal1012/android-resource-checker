803a62f0616d66ec54d184fc83e92b37d4190b84
aagarwal1012 <aagarwal9782@gmail.com>
2019-06-27 23:26:14 +0530

    rfac: undo wildcard imports

--- src/main/java/org/checkerframework/checker/androidresource/qual/res/AnimatorRes.java
+++ src/main/java/org/checkerframework/checker/androidresource/qual/res/AnimatorRes.java
@@ -2,7 +2,10 @@ package org.checkerframework.checker.androidresource.qual.res;
 
 import org.checkerframework.framework.qual.SubtypeOf;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @SubtypeOf({AnyRes.class})
 @Retention(RetentionPolicy.RUNTIME)

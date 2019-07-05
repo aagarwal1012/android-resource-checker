4540f7750cd90aab081b2163147c003a917121c3
aagarwal1012 <aagarwal9782@gmail.com>
2019-07-06 03:25:23 +0530

    docs: added docs for annotation classes

--- src/main/java/org/checkerframework/checker/androidresource/qual/container/LayoutContainer.java
+++ src/main/java/org/checkerframework/checker/androidresource/qual/container/LayoutContainer.java
@@ -8,6 +8,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Represents <i>Layout</i> inner class of resource class {@code R}.
+ * <br> It is meant for internal use only. Programmers should rarely write this type.
+ */
 @SubtypeOf({ResourceTop.class})
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
--- src/main/java/org/checkerframework/checker/androidresource/qual/res/AnyRes.java
+++ src/main/java/org/checkerframework/checker/androidresource/qual/res/AnyRes.java
@@ -8,6 +8,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Represents a resource type. Use when the specific type of resource
+ * is not known but it must be a resource type.
+ * <br> Super type of all {@code @XXXRes}.
+ */
 @SubtypeOf({ResourceTop.class})
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})

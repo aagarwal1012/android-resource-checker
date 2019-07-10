884a216f37b7bd15a6bb3bc5b359416bf7616e34
aagarwal1012 <aagarwal9782@gmail.com>
2019-07-11 03:17:19 +0530

    ptch: added expected error message in the AndroidAliasesTest

--- tests/androidresource/src/AndroidAliasesTest.java
+++ tests/androidresource/src/AndroidAliasesTest.java
@@ -30,28 +30,35 @@ public class AndroidAliasesTest {
     void test() {
         init();
 
+        // :: error: (assignment.type.incompatible)
         any_res = a;
 
         b = raw_res;
 
         any_res = string_res;
 
-
+        // :: warning: (binary.operation.left.operand.found.resource)
         a = any_res + b;
 
+        // :: error: (assignment.type.incompatible)
         string_res = a - b;
 
+        // :: error: (assignment.type.incompatible) :: warning: (binary.operation.both.operand.found.resource)
         any_res = raw_res * string_res;
 
+        // :: error: (assignment.type.incompatible) :: warning: (binary.operation.right.operand.found.resource)
         raw_res = a / any_res;
 
-
+        // :: warning: (compound.assignment.expression.found.resource)
         a += string_res;
 
+        // :: error: (assignment.type.incompatible) :: warning: (compound.assignment.both.found.resource)
         raw_res -= any_res;
 
+        // :: warning: (compound.assignment.both.found.resource)
         any_res *= string_res;
 
+        // :: error: (assignment.type.incompatible) :: warning: (compound.assignment.variable.found.resource)
         string_res /= b;
     }
 }
